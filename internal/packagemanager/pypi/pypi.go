500818f33d6e272a8279059e81d986df2391c77b
Ruben J. Jongejan <ruben.jongejan@gmail.com>
2024-11-27 15:29:45 +0100

    Hopefully fix check

--- internal/packagemanager/pypi/pypi.go
+++ internal/packagemanager/pypi/pypi.go
@@ -348,7 +348,7 @@ func (p *PyPI) CheckEndpoint() error {
 	var testURL string
 	if p.isCustomIndexURL {
 		// For custom index URLs, try to access the base URL
-		testURL = p.pypiURL + "/pip"
+		testURL = fmt.Sprintf("%s/%s/", p.pypiURL, "pip")
 	} else {
 		// For PyPI, try to access a known package
 		testURL = p.pypiURL + "/pip/json"
